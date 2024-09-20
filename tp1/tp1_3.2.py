6f94c1000779f37e477d2fa2957062c8b6eaa46a
hapuque <karen.ferreira@icomp.ufam.edu.br>
2024-09-20 01:09:29 -0400

    join bd to tp1_3.2 and create a conection in tp1_3.3 with the database

--- tp1/tp1_3.2.py
+++ tp1/tp1_3.2.py
@@ -1,7 +1,15 @@
 import sys
 import re
-import bd
 import threading
+
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extras import execute_values
+
+host = 'localhost'
+user = 'postgres'
+password = '1234'
+db_name = 'tp1bd'
 
 def process_line(line):
     parts = line.replace(':','').split()
@@ -216,6 +224,335 @@ def process_file(input_file):
 
     return products, list(categories), similars, prods_categories, prods_subcategories, prods_reviews
 
+# Função para conectar a um banco de dados específico
+def connect_to_db(host, user, password, dbname):
+    return psycopg2.connect(host=host, user=user, password=password, dbname=dbname)
+
+def create_product_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+    
+    # SQL para criar a tabela
+    create_table_query = """
+    CREATE TABLE product (
+        assin VARCHAR (20) PRIMARY KEY,
+        title VARCHAR(500),
+        "group" VARCHAR(50),
+        salerank INTEGER
+    );
+    """
+    
+    try:
+        # Executar o comando SQL
+        cur.execute(create_table_query)
+        #print("Tabela 'product' criada com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+
+def create_category_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+
+    # SQL para criar a tabela category
+    create_table_query = """
+    CREATE TABLE category (
+        category_key INTEGER PRIMARY KEY,
+        name VARCHAR(60) NOT NULL
+    );
+    """
+    
+    try:
+        # Executar o comando SQL para criar a tabela
+        cur.execute(create_table_query)
+        #print("Tabela 'category' criada com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a tabela 'category': {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+ 
+
+def create_product_category_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+
+    # SQL para criar a tabela product_category
+    create_table_query = """
+    CREATE TABLE product_category (
+        assin VARCHAR(20),
+        id_category INTEGER,
+        PRIMARY KEY (assin, id_category),
+        FOREIGN KEY (assin) REFERENCES product(assin),
+        FOREIGN KEY (id_category) REFERENCES category(category_key)
+
+    );
+    """
+    
+    try:
+        # Executar o comando SQL para criar a tabela
+        cur.execute(create_table_query)
+        #print("Tabela 'product_category' criada com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a tabela 'product_category': {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+
+
+def create_similar_product_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+
+    # SQL para criar a tabela similar
+    create_table_query = """
+    CREATE TABLE similar_product (
+        assin VARCHAR(20),
+        assin_similar VARCHAR(20),
+        PRIMARY KEY (assin, assin_similar),
+        FOREIGN KEY (assin) REFERENCES product(assin)
+    );
+    """
+    
+    try:
+        # Executar o comando SQL para criar a tabela
+        cur.execute(create_table_query)
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a tabela 'similar_product': {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+
+def create_product_subcategory_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+
+    # SQL para criar a tabela product_subcategory
+    create_table_query = """
+    CREATE TABLE product_subcategory (
+        assin VARCHAR(20),
+        id_category INTEGER,
+        subcategory INTEGER,
+        PRIMARY KEY (assin, id_category, subcategory),
+        FOREIGN KEY (assin, id_category) REFERENCES product_category(assin, id_category),
+        FOREIGN KEY (subcategory) REFERENCES category(category_key )
+    );
+    """
+    
+    try:
+        # Executar o comando SQL para criar a tabela
+        cur.execute(create_table_query)
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a tabela 'product_subcategory': {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+
+def create_review_table():
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    # Abrir um cursor para realizar operações no banco de dados
+    cur = con.cursor()
+
+    # SQL para criar a tabela review
+    create_table_query = """
+    CREATE TABLE review (
+        id SERIAL PRIMARY KEY,
+        assin VARCHAR(20),
+        ano INTEGER,
+        mes INTEGER,
+        dia INTEGER,
+        costumer VARCHAR(20),
+        rating INTEGER,
+        votes INTEGER,
+        helpful INTEGER,
+        FOREIGN KEY (assin) REFERENCES product(assin)
+    );
+    """
+    
+    try:
+        # Executar o comando SQL para criar a tabela
+        cur.execute(create_table_query)
+    except Exception as e:
+        print(f"Ocorreu um erro ao criar a tabela 'review': {e}")
+    finally:
+        # Fechar o cursor
+        cur.close()
+        con.close()
+
+def check_and_create_db():
+    con = connect_to_db(host, user, password, 'postgres')  # Conectar ao banco 'postgres'
+    con.autocommit = True
+    cur = con.cursor()
+
+    try:
+        # Verificar se o banco de dados já existe
+        cur.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), [db_name])
+        
+        if not cur.fetchone():
+            # Se não existir, criar o banco de dados
+            cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name)))
+            print(f"Banco de dados '{db_name}' criado com sucesso.")
+        else:
+            print(f"O banco de dados '{db_name}' já existe.")
+        
+    finally:
+
+        cur.close()
+        con.close()
+
+    # Retornar a nova conexão ao banco de dados (novo ou existente)
+    return 
+
+def insert_product(dados):
+    # Conectar ao banco de dados
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+    
+    # SQL para inserir dados na tabela product
+    insert_query = """
+    INSERT INTO product (assin, title, "group", salerank)
+    VALUES %s;
+    """
+    
+    try:
+        # Executar o comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Produto inserido com sucesso.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir o produto: {e}")
+    finally:
+        # Fechar o cursor e a conexão
+        cur.close()
+        con.close()
+
+def insert_product_category(dados):
+    #Conectar ao bd
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
+    #query para inserção
+    insert_query = """
+    INSERT INTO product_category (assin, id_category)
+    VALUES %s;
+    """
+
+    try:
+        #executar comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Product_category inserido com sucesso")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir product_category: {e}")
+    finally:
+        cur.close()
+        con.close()
+   
+def insert_category(dados):
+    #Conectar ao bd
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
+    #query para inserção
+    insert_query = """
+    INSERT INTO category (category_key, name)
+    VALUES %s;
+    """
+
+    try:
+        #executar comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Category inserido com sucesso")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir category: {e}")
+    finally:
+        cur.close()
+        con.close()
+
+def insert_similar(dados):
+    #Conectar ao bd
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
+    #query para inserção
+    insert_query = """
+    INSERT INTO similar_product (assin, assin_similar)
+    VALUES %s;
+    """
+
+    try:
+        #executar comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Similar inserido com sucesso")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir Similar: {e}")
+    finally:
+        cur.close()
+        con.close()
+
+def insert_product_subcategory(dados):
+    #Conectar ao bd
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
+    #query para inserção
+    insert_query = """
+    INSERT INTO product_subcategory (assin, id_category, subcategory)
+    VALUES %s;
+    """
+
+    try:
+        #executar comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Product_subcategory inserido com sucesso")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir product_subcategory: {e}")
+    finally:
+        cur.close()
+        con.close()
+
+def insert_review(dados):
+    #Conectar ao bd
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
+    #query para inserção
+    insert_query = """
+    INSERT INTO review (assin, ano, mes, dia, costumer, rating, votes, helpful)
+    VALUES %s;
+    """
+
+    try:
+        #executar comando de inserção
+        execute_values(cur, insert_query, dados)
+        #print("Review inserido com sucesso")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+    finally:
+        cur.close()
+        con.close()
 
 # Função para gerenciar as threads
 def insert_concurrently(function_names, dados_list):
@@ -223,11 +560,18 @@ def insert_concurrently(function_names, dados_list):
 
     # Iterar pelos nomes das funções e dados correspondentes
     for i, function_name in enumerate(function_names):
-        # Usar getattr para obter a função do módulo bd a partir do nome
-        insert_function = getattr(bd, "insert_"+function_name)
+        function = "insert_" + function_name
+        insert_function = globals().get(function)
+
+        # Verifica se a função foi encontrada
+        if insert_function is None:
+            print(f"Erro: A função '{function}' não foi encontrada.")
+            return
+    
 
         dados = dados_list[i]
         tam_dados = len(dados)
+
         # Criar uma thread para cada função de inserção, passando os dados correspondentes
         thread = threading.Thread(target=insert_function, args=(dados[0:(tam_dados//2)],))
         threads.append(thread)
@@ -245,14 +589,13 @@ def insert_concurrently(function_names, dados_list):
 
 if __name__ == "__main__":
     input_file = sys.argv[1]
-
-    bd.check_and_create_db()
-    bd.create_product_table()
-    bd.create_category_table()
-    bd.create_product_category_table()
-    bd.create_review_table()
-    bd.create_similar_product_table()
-    bd.create_product_subcategory_table()
+    check_and_create_db()
+    create_product_table()
+    create_category_table()
+    create_product_category_table()
+    create_review_table()
+    create_similar_product_table()
+    create_product_subcategory_table()
     product, category, similar, product_category, product_subcategory, review = process_file(input_file) 
     
     função = ["product", "category"]
@@ -267,46 +610,5 @@ if __name__ == "__main__":
     dados = [product_subcategory, review]
     insert_concurrently(função, dados)
 
-""" 
-    print(f"quantidade de produtos: {len(product)}")
-    print(f"quantidade de categories: {len(category)}")
-    print(f"quantidade de produtos_categoria: {len(product_category)}")
-    print(f"quantidade de similares: {len(similar)}")
-    print(f"quantidade de subcat: {len(product_subcategory)}")
-    print(f"quantidade de reviews: {len(review)}")
-     """
-"""     print("PRODUCTS")
-    for i in products:
-        print(i)
-    print('\n')
+    print("Banco de Dados Populado com Sucesso!")
 
-    print("CATEGORIES")
-    for i in categories_list:
-        print(i)
-    print('\n')
-
-    
-    print("SIMILARS")
-    for i in similars:
-        print(i)
-    print('\n')
-
-    
-    print("MAIN_CATEGORIES")
-    for i in prods_categories:
-        print(i)
-    print('\n')
-
-    
-    print("SUBCATEGORIES")
-    for i in prods_subcategories:
-        print(i)
-    print('\n')
-
-    
-    print("REVIEWS")
-    for i in prods_reviews:
-        print(i) """
-
-
-    
--- tp1/tp1_3.3.py
+++ tp1/tp1_3.3.py
@@ -1,52 +1,282 @@
-""" #conection
-import sqlite3
-conn = sqlite3.connect('database.db')
+from bd import connect_to_db
 
-#database query 
-customer_id = 123
-query = "SELECT products.name FROM products JOIN orders ON products.id = orders.product_id WHERE orders.customer_id = ?"
+host = 'localhost'
+user = 'postgres'
+password = '1234'
+db_name = 'tp1bd'
+
+def option_1(cur):
+    #query para inserção
+    #Menor avaliação
+    consult_query = """
+    (SELECT COSTUMER, HELPFUL, RATING
+    FROM REVIEW
+    WHERE ASSIN = '0670672238'
+    ORDER BY HELPFUL DESC, RATING ASC LIMIT 5);
+    """
+
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                print(f"COSTUMER: {row[0]}, HELPFUL: {row[1]}, RATING: {row[2]}")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+    consult_query = """
+    (SELECT COSTUMER, HELPFUL, RATING
+    FROM REVIEW
+    WHERE ASSIN = 'B00000C2CU'
+    ORDER BY HELPFUL DESC, RATING DESC LIMIT 5);
+    """
+
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                print(f"COSTUMER: {row[0]}, HELPFUL: {row[1]}, RATING: {row[2]}")
+        else:
+            print("Nenhum resultado encontrado.")
     
-#executation
-cursor = conn.cursor()
-cursor.execute(query, (customer_id,))
-results = cursor.fetchall()
-      
-
- """
-
-query_1 = ""
-query_2 = ""
-query_3 = ""
-query_4 = ""
-query_5 = ""
-query_6 = ""
-query_7 = ""
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
 
 
 
-def option_1(query_1):
-    print("executando função 1")
+def option_2(cur):
+    #query para inserção
+    consult_query = """
+    SELECT p2.assin AS similar_assin, 
+       p2.title AS similar_title, 
+       p2.salerank AS similar_salerank
+    FROM similar_product sp
+    JOIN product p1 ON sp.assin = p1.assin
+    JOIN product p2 ON sp.assin_similar = p2.assin
+    WHERE p1.assin = '1559362022'  -- O produto original
+    AND p2.salerank < p1.salerank;  -- Comparação do salerank
 
-def option_2(query_2):
-    print("executando função 2")
+    """
 
-def option_3(query_3):
-    print("executando função 3")
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
 
-def option_4(query_4):
-    print("executando função 4")
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            print("| similar_assin | similar_title | similar_salerank |")
+            print("|---------------|---------------|------------------|")
+            for row in results:
+                similar_assin, similar_title, similar_salerank = row
+                print(f"| {similar_assin} | {similar_title} | {similar_salerank} |")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
 
-def option_5(query_5):
-    print("executando função 5")
 
-def option_6(query_6):
-    print("executando função 6")
+def option_3(cur):
+    #query para inserção
+    consult_query = """
+        SELECT 
+            DATE(r.ano || '-' || r.mes || '-' || r.dia) AS review_date,
+            AVG(r.rating) AS avg_daily_rating
+        FROM 
+            review r
+        WHERE 
+            r.assin = '1559362022'
+        GROUP BY 
+            review_date
+        ORDER BY 
+            review_date ASC;
+    """
 
-def option_7(query_7):
-    print("executando função 7")
-     
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                date, avg= row
+                print(f"| {date} | {avg}")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+
+def option_4(cur):
+    #query para inserção
+    consult_query = """
+    WITH RankedProducts AS (
+        SELECT 
+            assin,
+            title,
+            "group",
+            salerank,
+            ROW_NUMBER() OVER (PARTITION BY "group" ORDER BY salerank) AS rank
+        FROM 
+            product
+    )
+
+    SELECT 
+        assin,
+        title,
+        "group",
+        salerank
+    FROM 
+        RankedProducts
+    WHERE 
+        rank <= 10
+    ORDER BY 
+        "group", rank;
+
+
+    """
+
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                assin, title, group, salerank = row
+                print(f"| {assin} | {title} | {group} | {salerank} |")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+
+def option_5(cur):
+    #query para inserção
+    consult_query = """
+        SELECT 
+            p.title AS product_title, 
+            AVG(r.helpful) AS avg_helpful_reviews
+        FROM 
+            review r
+        JOIN 
+            product p ON r.assin = p.assin
+        GROUP BY 
+            p.title
+        ORDER BY 
+            avg_helpful_reviews DESC
+        LIMIT 10;
+    """
+
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                tittle, avg = row
+                print(f"| {tittle} | {avg} ")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+
+def option_6(cur):
+    #query para inserção
+    consult_query = """
+        SELECT 
+        c.name AS category_name,
+        AVG(r.helpful) AS avg_helpful_reviews
+        FROM 
+            review r
+        JOIN 
+            product_category pc ON r.assin = pc.assin
+        JOIN 
+            category c ON pc.id_category = c.category_key
+        GROUP BY 
+            c.name
+        ORDER BY 
+            avg_helpful_reviews DESC
+        LIMIT 5;
+    """
+
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                category, avg = row
+                print(f"| {category} | {avg} ")
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+
+def option_7(cur):
+    consult_query = """
+        SELECT 
+            p."group", 
+            r.costumer, 
+            COUNT(r.id) AS total_reviews
+        FROM 
+            review r
+        JOIN 
+            product p ON r.assin = p.assin
+        GROUP BY 
+            p."group", r.costumer
+        ORDER BY 
+            total_reviews DESC
+        LIMIT 10;
+    """
+    try:
+        #executar comando de inserção
+        cur.execute(consult_query)
+        results = cur.fetchall()  # Use fetchone() se você espera apenas uma linha
+
+        # Verificar se resultados foram retornados
+        if results:
+            print(f"results {results}")
+            for row in results:
+                group, costumer, total_reviews = row
+                print(f'{group} | {costumer} | {total_reviews}')
+           
+        else:
+            print("Nenhum resultado encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao inserir review: {e}")
+
+
 
 if __name__ == '__main__':
+    con = connect_to_db(host, user, password, db_name)
+    con.autocommit = True
+    cur = con.cursor()
+
     condition = True
     while(condition):
         print("1 - Listar os 5 comentários mais úteis e com maior avaliação e os 5 comentários mais úteis e com menor avaliação")
@@ -64,19 +294,22 @@ if __name__ == '__main__':
             case 0:
                 condition =False
             case 1:
-                option_1()
+                option_1(cur)
             case 2:
-                option_2()
+                option_2(cur)
             case 3:
-                option_3()
+                option_3(cur)
             case 4:
-                option_4()
+                option_4(cur)
             case 5:
-                option_5()
+                option_5(cur)
             case 6:
-                option_6()
+                option_6(cur)
             case 7:
-                option_7()
+                option_7(cur)
             case _:
                 print("Opção inválida, escolhe uma opção válida")
                 print("\n")
+
+    cur.close()
+    con.close()
