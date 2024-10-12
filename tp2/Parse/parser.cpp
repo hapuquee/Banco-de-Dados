c66233012d851b3e4da4dbf599699c060b9b2e9d
hapuquee <karen.ferreira@icomp.ufam.edu>
2024-10-11 21:52:56 -0400

    mudancas de nome de variaveis

--- tp2/Parse/parser.cpp
+++ tp2/Parse/parser.cpp
@@ -10,8 +10,7 @@
 using namespace std;
 
 // Função para remover aspas em um stream de entrada
-//quot -> aspas
-void clean_quot(istream& stream, string& resultado) {
+void remover_aspas(istream& stream, string& resultado) {
     resultado.clear();
     char ch;
 
@@ -25,17 +24,17 @@ void clean_quot(istream& stream, string& resultado) {
 }
 
 // Função para normalizar uma string (remove caracteres inválidos, múltiplos espaços)
-string format_str(string str) {
+string formatar_str(string str) {
     // Remove caracteres não ASCII, novas linhas e aspas
     str.erase(remove_if(str.begin(), str.end(), [](unsigned char c) {
         return c > 127 || c == '\n' || c == '\"';
     }), str.end());
 
     // Remove múltiplos espaços
-    auto new_end = unique(str.begin(), str.end(), [](char left, char right) {
-        return isspace(static_cast<unsigned char>(left)) && isspace(static_cast<unsigned char>(right));
+    auto novo_final = unique(str.begin(), str.end(), [](char esquerda, char direita) {
+        return isspace(static_cast<unsigned char>(esquerda)) && isspace(static_cast<unsigned char>(direita));
     });
-    str.erase(new_end, str.end());
+    str.erase(novo_final, str.end());
 
     // Remove espaços no início e no fim
     str.erase(0, str.find_first_not_of(' '));  // Remove espaços do início
@@ -44,7 +43,7 @@ string format_str(string str) {
     return str;
 }
 
-void ler_arquivo_csv(const string& nome_arquivo)
+void processa_arqv(const string& nome_arquivo)
 {
     ifstream arquivo(nome_arquivo, ios::in);
     
@@ -59,69 +58,69 @@ void ler_arquivo_csv(const string& nome_arquivo)
     while (getline(arquivo, linha))
     {
         id_str++;
-        stringstream linha_analisada(linha);
+        stringstream linha_processada(linha);
         string dado;
     
-        int id, year, citations;
-        string title, authors, update, snippet;
+        int id, ano, citacoes;
+        string titulo, autores, atualizacao, snippet;
 
         try
         {
             // atualiza o campo id apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
             id = stoi(dado);
 
             // atualiza o campo titulo apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
-            title = dado;
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
+            titulo = dado;
 
             // atualiza o campo ano apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
-            year = stoi(dado);
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
+            ano = stoi(dado);
 
             // atualiza o campo autores após a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
-            authors = dado;
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
+            autores = dado;
 
             // atualiza o campo citacoes apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
-            citations = stoi(dado);
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
+            citacoes = stoi(dado);
 
-            // atualiza o campo update apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
-            update = dado;
+            // atualiza o campo atualizacao apos a remocao das aspas
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
+            atualizacao = dado;
 
             // atualiza o snippet apos a remocao das aspas
-            getline(linha_analisada, dado, '"');
-            clean_quot(linha_analisada, dado);
+            getline(linha_processada, dado, '"');
+            remover_aspas(linha_processada, dado);
             snippet = dado;
 
             // normaliza todas as strings
-            title = format_str(title);
-            authors = format_str(authors);
-            update = format_str(update);
-            snippet = format_str(snippet);
+            titulo = formatar_str(titulo);
+            autores = formatar_str(autores);
+            atualizacao = formatar_str(atualizacao);
+            snippet = formatar_str(snippet);
 
             REGISTRO registro;
             try{
-                criarRegistro(registro, id, title, year,authors,citations,update,snippet);
+                criarRegistro(registro, id, titulo, ano,autores,citacoes,atualizacao,snippet);
                 //exibirRegistro(registro);
             }
             catch(const invalid_argument& e){
                 cerr<< "ERRO AO CRIAR REGISTRO NA LINHA: " << id<< endl;
             }
 
-          /*   Registro* novo_registro = criar_registro(id, title, year, authors, citations, update, snippet);
+          /*   Registro* novo_registro = criar_registro(id, titulo, ano, autores, citacoes, atualizacao, snippet);
             size_t posicao = hash.inserir_registro_bucket(novo_registro);
 
             RegistroBPT* reg1 = new RegistroBPT(id, posicao);
-            RegistroString* reg2 = new RegistroString(title, posicao);
+            RegistroString* reg2 = new RegistroString(titulo, posicao);
 
             arvore1.inserir_arvore(reg1);
             arvore2.inserir_arvore_s(reg2);
@@ -149,7 +148,7 @@ int main() {
     // Solicitar o nome do arquivo ao usuário
     cout << "Digite o nome do arquivo CSV: ";
     cin >> nome_arquivo;
-    ler_arquivo_csv(nome_arquivo);
+    processa_arqv(nome_arquivo);
 
     return 0;
 }
